f1ce6972100f2461668035874e183fe52c6125b1
Seracrooce <fschmidt@live.com>
2021-12-14 11:59:47 -0600

    made the basic structure for our state machine. Will have to add events later

---
+++ src/scripts/GameplayArea/GameModules/Game.js
@@ -0,0 +1,57 @@
+
+
+export const gameState = {
+    //possible states GameSelect, NewGame, and ActiveGame
+    state: 'GameSelect',
+    transitions: {
+        GameSelect: {
+            newGame() {
+                //change state
+                this.state = 'NewGame'
+                //make a statechange event to rerender I think the ones from gameselect will change this elements innerhtml. finished games should change change most html.
+            },
+
+            continueGame() {
+                this.state = 'ActiveGame'
+            }
+        },
+        
+        NewGame: {
+            gameSelect() {
+                this.state = 'GameSelect'
+            }
+        },
+
+        ActiveGame: {
+            gameSelect() {
+                this.state = 'GameSelect'
+            }
+        }
+    },
+    changeState(actionName) {
+        const action = this.transitions[this.state][actionName];
+
+        if (action) {
+            action.call(this);
+        } else {
+            console.log('Invalid Action');
+        }
+    }
+};
+
+export const Game = () => {
+    switch (gameState.state) {
+
+        case "GameSelect" :
+            //run game select funtion
+            break;
+
+        case "NewGame" :
+            //run new game function
+            break;
+
+        case "ActiveGame" :
+            //run active game function
+            break;
+    }
+}
