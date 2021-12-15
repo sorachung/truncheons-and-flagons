70375a4d747f6d84c189118f6cb945791194c07a
Seracrooce <fschmidt@live.com>
2021-12-15 11:25:38 -0600

    modularized renders on teamsAndResults changes. New custom events and eventListeners were utilized. Additionally a TeamsAndResults Module was created to handle those.

---
+++ src/scripts/TeamsAndResults/TeamsAndResults.js
@@ -0,0 +1,36 @@
+import { RecentGamesList } from "./RecentGamesList.js"
+import { JoinTeam } from "./TeamStuff/JoinTeam.js";
+import { CreateTeam } from "./TeamStuff/CreateTeam.js" 
+import { TeamList } from "./TeamStuff/TeamList.js";
+import { fetchPlayers, fetchTeams } from "../dataAccess.js";
+
+export const TeamsAndResults = () => {
+
+    return `
+    <section class="teamFormation teamsAndResultsChunk">
+        <div class="teamFormationChunk teamCreation">
+            ${CreateTeam()}
+        </div>
+        <div class="teamFormationChunk joinTeam">
+            ${JoinTeam()}
+        </div>
+        <div class="teamFormationChunk incompleteTeamList">
+            ${TeamList()}
+        </div>
+    </section>
+    <section class="recentGameResults teamsAndResultsChunk">
+            ${RecentGamesList()}
+    </section>`
+}
+
+//grab container
+const mainContainer = document.querySelector(".container");
+
+//make our listener that rerenders this area only
+mainContainer.addEventListener("teamStateChanged", customEvent => {
+    Promise.all([fetchPlayers(), fetchTeams()])
+        .then(() => {
+            const teamEl = document.querySelector(".teamsAndResults");
+            teamEl.innerHTML = TeamsAndResults();
+        })
+})
