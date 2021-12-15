746500c3b7a5469a9986187748f49d75017a0864
sorachung <sorachung.src@gmail.com>
2021-12-15 16:59:55 -0600

    team roster shows up when clicking team name

--- src/scripts/TeamsAndResults/TeamStuff/TeamRostersList.js
+++ src/scripts/TeamsAndResults/TeamStuff/TeamRostersList.js
@@ -2,15 +2,46 @@ import { getPlayers, getTeams } from "../../dataAccess.js";
 
 export const TeamRostersList = () => {
     const teams = getTeams();
-    let html = `<ul class="teamRostersList">`
-    html = teams.map(team => {
-        const playersInTeam = players.filter(player => player.teamId === team.id)
-        return `
+    const players = getPlayers();
+
+    let html = `<ul class="teamRostersListUl">`;
+    html += teams
+        .map((team) => {
+            const playersInTeam = players.filter(
+            (player) => player.teamId === team.id
+            );
+            return `
             <li class="teamRostersListItem" id="teamRosterListItem--${team.id}">
                 ${team.name}
-            </li>`
-    }).join("")
-    html += `</ul>`
+                <div class="teamRosterPopUp" id="teamRosterPopUp--${team.id}">
+                <ul class=teamRosterPlayers>
+                    ${playersInTeam.length > 0 ? playersInTeam.map((player) => `<li>${player.firstName} ${player.lastName}</li>`).join("") : `<li>no players</li>`}
+                </ul>    
+                
+                </div>
+            </li>`;
+        })
+        .join("");
+    html += `</ul>`;
 
     return html;
-}
+};
+
+const mainContainer = document.querySelector(".container");
+mainContainer.addEventListener("click", (clickEvent) => {
+    if (clickEvent.target.id.startsWith("teamRosterListItem--")) {
+        const [, teamId] = clickEvent.target.id.split("--");
+        const popup = document.getElementById(`teamRosterPopUp--${teamId}`)
+
+        const showPopups = document.getElementsByClassName("show")
+        if(showPopups.length > 0 ) {
+            for (const showPopup of showPopups) {
+                if(showPopup.id != popup.id){
+                    showPopup.classList.remove("show")
+                }
+            }
+        }
+        popup.classList.toggle("show");
+        
+    }
+});
