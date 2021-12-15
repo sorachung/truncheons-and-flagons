aa7b7a713729a7ae1a6e05a6664a9ebca898bb65
sorachung <sorachung.src@gmail.com>
2021-12-14 19:40:27 -0600

    scrolling now happens from the right and repeats once everythings gone past the left side of the screen

--- src/scripts/SeasonScoreBanner.js
+++ src/scripts/SeasonScoreBanner.js
@@ -2,10 +2,11 @@ import { getTeams } from "./dataAccess.js";
 
 export const SeasonScoreBanner = () => {
     const teams = getTeams();
+    const sortedTeams = teams.sort((team1, team2) => team2.seasonScore - team1.seasonScore);
     let html = `<article class="ticker">`
 
-    const scrollingText = teams.map(team => {
-        return `<div class="teamTickerItem" id="team--${team.id}">${team.name}: ${team.seasonScore}</div>`
+    const scrollingText = sortedTeams.map(team => {
+        return `<div class="teamTickerItem">${team.name}: ${team.seasonScore}</div>`
     }).join("")
 
     html += scrollingText + `</article>`
