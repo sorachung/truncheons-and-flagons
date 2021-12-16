f9545434cf798b2a2cdb4109c1294a77e6a5e5b3
Seracrooce <fschmidt@live.com>
2021-12-16 17:17:16 -0600

    various style changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,18 @@
 import { fetchGames, fetchPlayers, fetchScores, fetchTeams, fetchPositions } from "./dataAccess.js";
 import { SeasonScoreBanner } from "./SeasonScoreBanner.js";
 import { TruncheonsAndFlagons } from "./TruncheonsAndFlagons.js";
+import { Footer } from "./Footer.js";
 
 const mainContainer = document.querySelector(".container");
-const headerContainer = document.querySelector(".header")
+const headerContainer = document.querySelector(".header");
+const footerContainer = document.querySelector(".footer");
 
 const render = () => {
     Promise.all([fetchPlayers(), fetchTeams(), fetchGames(), fetchScores(), fetchPositions()])
     .then( () => {
         mainContainer.innerHTML = TruncheonsAndFlagons();
         headerContainer.innerHTML = SeasonScoreBanner();
+        footerContainer.innerHTML = Footer();
     })
 };
 
